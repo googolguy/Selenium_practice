6da8424d2192b0569a92d4bb07d2031d1ad510f2
googolguy <piyush15feb.yadav@gmail.com>
2020-02-09 22:28:16 +0530

    Added 2nd lecture of selenium

---
+++ 2_webdriver_commands.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 21:05:49 2020
+
+@author: aditipc
+
+Description:
+"""
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path="C:\Driver\chromedriver_win32\chromedriver.exe")
+
+# To open a webpage
+driver.get("http://demo.automationtesting.in/Windows.html")
+
+# To get the title of the page
+print(driver.title)
+
+# To get the URL of the webpage
+print(driver.current_url)
+
+# To click a button by selection it using xpath
+driver.find_element_by_xpath("//*[@id='Tabbed']/a/button").click()
+
+# To pause the execution for some time(sec)
+time.sleep(5)
+
+# To close the driver(Parent Browser tab)
+driver.close()
+'''The button clicked will open a new tab in Browser
+but the close command will only close the parent tab'''
+
+# To close all the Browser tabs
+driver.quit()
